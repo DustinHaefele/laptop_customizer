18a1a1dd7c4a784bd4a200ae8de59233bf0ac0ba
Dustin Haefele <Dustin.haefele@gmail.com>
2019-05-23 20:17:20 -0400

    FeaturePriceSummary and FeaturePrice Created and refactored from App.js

--- src/FeaturePrice/FeaturePrice.js
+++ src/FeaturePrice/FeaturePrice.js
@@ -4,16 +4,16 @@ import React from "react";
 export default function FeaturePrice(props) {
   
     return (
-      <div className="summary__option" key={props.feature}>
-        <div className="summary__option__label">{props.feature} </div>
+      <div className="summary__option" key={props.key}>
+        <div className="summary__option__label">{props.key} </div>
         <div className="summary__option__value">
-          {props.feature.name}
+          {props.name}
         </div>
         <div className="summary__option__cost">
           {new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD"
-          }).format(props.feature.cost)}
+          }).format(props.cost)}
         </div>
       </div>
     );
--- src/FeaturePriceSummary/FeaturePriceSummary.js
+++ src/FeaturePriceSummary/FeaturePriceSummary.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import FeaturePrice from '../FeaturePrice/FeaturePrice';
+import React from "react";
+import FeaturePrice from "../FeaturePrice/FeaturePrice";
 
+function FeaturePriceSummary(props) {
+  const total = Object.keys(props.selected).reduce(
+    (acc, curr) => acc + props.selected[curr].cost,
+    0
+  );
 
-
-function FeaturePriceSummary(props){
-
-  // Object.keys(this.state.selected).map(key => (
-  const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
   return (
-  <section className="main__summary">
-  <h3>NEW GREENLEAF 2018</h3>
-  <FeaturePrice 
-  price ={props.price}/>
-  <div className="summary__total">
-    <div className="summary__total__label">Your Price: </div>
-    <div className="summary__total__value">
-      {new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-      }).format(total)}
-    </div>
-  </div>
-</section>
-);
+    <section className="main__summary">
+      <h3>NEW GREENLEAF 2018</h3>
 
+      {Object.keys(props.selected).map(key => {
+        return <FeaturePrice key={key} 
+          cost={props.selected[key].cost} 
+          name={props.selected[key].name} />;
+      })}
+
+      <div className="summary__total">
+        <div className="summary__total__label">Your Price: </div>
+        <div className="summary__total__value">
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+          }).format(total)}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default FeaturePriceSummary;
